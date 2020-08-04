dfec480fb956689d7d71fdfbcb1fcf8c6ebb810b
eukurapov <eukurapov@gmail.com>
2020-08-04 16:48:53 +0300

    LocationListView refactoring and comments

--- Weather/Views/LocationListView.swift
+++ Weather/Views/LocationListView.swift
@@ -11,103 +11,57 @@ import CoreData
 import Combine
 
 struct LocationListView: View {
-
-    @FetchRequest(fetchRequest: Location.fetchRequest(.all)) var locations: FetchedResults<Location>
-    @ObservedObject var weatherFetcher: WeatherFetcher = WeatherFetcher()
     
+    // property wrapper to “observe” coredata request results — i.e. results list updates once something changed in DB
+    @FetchRequest(fetchRequest: Location.fetchRequest(.all)) var locations: FetchedResults<Location>
+    // helpers to fetch weather using OpenWeather API and to get current user location
+    @ObservedObject var weatherFetcher: WeatherFetcher = WeatherFetcher()
     @ObservedObject var locationFetcher = LocationFetcher()
     
-    @State var showCitySearch: Bool = false
+    // states to show related sheets
+    @State var showLocationSearch: Bool = false
     @State var showWeatherMap: Bool = false
     
-    @Environment(\.editMode) var editMode
     // fix for Navigaitionview being disabled after editor is closed by button
     @Environment(\.presentationMode) var presentationMode
+    // context to work with CoreData
     @Environment(\.managedObjectContext) var context
+    // edit mode controlled by Edit button in NavigationBar
+    @Environment(\.editMode) var editMode
     
-//    @State var selectedItem: Location?
+    //    @State var selectedItem: Location?
     
     var body: some View {
-         NavigationView {
+        NavigationView {
             VStack {
                 List {
-                    ForEach(locations) { location in
-                        LocationListItem(location: location)
-                        .id(UUID()) // fix for extra animation when moving an item up in a list (see developer.apple.com/forums/thread/133134)
-//                        .onTapGesture {
-//                            if self.selectedItem != location {
-//                                self.selectedItem = location
-//                            } else {
-//                                self.selectedItem = nil
-//                            }
-//                        }
-//                        if self.selectedItem == location {
-//                            HStack {
-//                                Text("Here could be a feorecast")
-//                            }.frame(minHeight: 120)
-//                        }
-                    }
-                    .onMove(perform: move)
-                    .onDelete(perform: remove)
+                    ForEach(locations) { location in self.locationItem(for: location) }
+                        .onMove(perform: move)
+                        .onDelete(perform: remove)
                 }
             }
             .navigationBarTitle("Weather")
             .navigationBarItems(
                 leading: EditButton(),
-                trailing: HStack {
-                    Button(
-                        action: {
-                            self.showWeatherMap = true
-                    },
-                        label: {
-                            Image(systemName: "map")
-                                .imageScale(.medium)
-                                .padding(.horizontal, 5)
-                    })
-                    .sheet(isPresented: $showWeatherMap) {
-                        NavigationView {
-                            MapView(annotations: Array(self.locations), center: self.locations.first(where: { $0.isCurrent }))
-                            .edgesIgnoringSafeArea(.all)
-                            .navigationBarTitle("Map", displayMode: .inline)
-                            .navigationBarItems(trailing: Button("Done", action: { self.showWeatherMap = false }))
-                        }
-                    }
-                    Button(
-                        action: {
-                            self.updateLocations()
-                    },
-                        label: {
-                            Image(systemName: "arrow.clockwise")
-                                .imageScale(.medium)
-                                .padding(2)
-                    })
-                    Button(
-                        action: {
-                            self.showCitySearch = true
-                    },
-                        label: {
-                            Image(systemName: "plus").imageScale(.large).padding(2)
-                    })
-                    .sheet(isPresented: $showCitySearch) {
-                        LocationSearch(isShown: self.$showCitySearch) { searchResult in
-                            Location.from(searchResult, context: self.context, source: .manual)
-                        }
-                        .environmentObject(self.weatherFetcher)
-                    }
-            })
+                trailing: trailingNavBarItems)
         }
         .onAppear {
-            self.locationFetcher.start()
-            self.updateLocations()
             UITableView.appearance().separatorStyle = .none
+            // start receiving user current location
+            self.locationFetcher.start()
+            // update weather for all locations
+            self.updateLocations()
         }
     }
     
+    // updates locations list, cancellable is requiered to keep sink return value out of function scope
     @State private var locationCancellable: AnyCancellable?
-    
     private func updateLocations() {
+        // fetch weather for all locations in list
         self.weatherFetcher.fetch(Array(self.locations), in: self.context)
+        // cancel existing subscription to current location updates if any
         locationCancellable?.cancel()
+        // subscribe to current user location updates to request location with weather for new coordinates
         self.locationCancellable = self.locationFetcher.currentLocation.sink { location in
             if let lastLocation = location {
                 self.weatherFetcher.fetchCurrentLocationAt(latitude: lastLocation.latitude, longitude: lastLocation.longitude, in: self.context)
@@ -126,6 +80,71 @@ struct LocationListView: View {
     private func remove(at offsets: IndexSet) {
         for index in offsets {
             locations[index].remove()
+        }
+    }
+    
+    private func locationItem(for location: Location) -> some View {
+        LocationListItem(location: location)
+            .id(UUID()) // fix for extra animation when moving an item up in a list (see developer.apple.com/forums/thread/133134)
+        // some code to add expanding view with forecast later (maybe)
+        //          .onTapGesture {
+        //                if self.selectedItem != location {
+        //                    self.selectedItem = location
+        //                } else {
+        //                    self.selectedItem = nil
+        //                }
+        //        }
+        //        if self.selectedItem == location {
+        //            HStack {
+        //                Text("Here could be a feorecast")
+        //            }.frame(minHeight: 120)
+        //        }
+    }
+    
+    private var trailingNavBarItems: some View {
+        HStack {
+            // button to show weather locations on map
+            Button(
+                action: {
+                    self.showWeatherMap = true
+            },
+                label: {
+                    Image(systemName: "map")
+                        .imageScale(.medium)
+                        .padding(.horizontal, 5)
+            })
+            .sheet(isPresented: $showWeatherMap) {
+                NavigationView {
+                    MapView(annotations: Array(self.locations), center: self.locations.first(where: { $0.isCurrent }))
+                        .edgesIgnoringSafeArea(.all)
+                        .navigationBarTitle("Map", displayMode: .inline)
+                        .navigationBarItems(trailing: Button("Done", action: { self.showWeatherMap = false }))
+                }
+            }
+            // update locations button
+            Button(
+                action: {
+                    self.updateLocations()
+            },
+                label: {
+                    Image(systemName: "arrow.clockwise")
+                        .imageScale(.medium)
+                        .padding(2)
+            })
+            // button to show locations search screen
+            Button(
+                action: {
+                    self.showLocationSearch = true
+            },
+                label: {
+                    Image(systemName: "plus").imageScale(.large).padding(2)
+            })
+            .sheet(isPresented: $showLocationSearch) {
+                LocationSearch(isShown: self.$showLocationSearch) { searchResult in
+                    Location.from(searchResult, context: self.context, source: .manual)
+                }
+                .environmentObject(self.weatherFetcher)
+            }
         }
     }
     
